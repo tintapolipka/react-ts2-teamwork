6f1a336717eb17734431284488ef27d54e75746a
tintapolipka <109940436+tintapolipka@users.noreply.github.com>
2024-10-31 19:11:28 +0100

    CSS rules added

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Üdvözöllek a React alkalmazásban!</h1>
+      <p>Ez egy alap App komponens TypeScript-tel.</p>
+    </div>
+  );
+};
+
+export default App;
