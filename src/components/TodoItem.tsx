41775bc6f0b87ffa2816100fa0611d13dcb10136
tintapolipka <109940436+tintapolipka@users.noreply.github.com>
2024-11-01 14:58:34 +0100

    State 'isDone' added to TodoItem

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import { Todo } from "../interfaces/Todo";
 
 const TodoItem: React.FC<Todo> = ({ title, done }) => {
+  const [isDone,setIsDone] = useState(done);
+  
+  function handleChangeCheckBox(){
+    setIsDone(!isDone);
+  }
+
   return (
     <div>
       <h1>{title}</h1>
       <label>
-        {done ?
-          (<input type="checkbox" checked />) : (<input type="checkbox" />)
+        {isDone ?
+          (<input onChange={handleChangeCheckBox} type="checkbox" defaultChecked />) : (<input onChange={handleChangeCheckBox} type="checkbox" />)
         }
       </label>
+      {isDone && <p>Done!</p>}
       <button>Delete</button>
     </div>
   )
