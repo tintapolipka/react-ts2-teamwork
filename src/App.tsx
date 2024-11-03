e2a41e9236b62997a2b4959da8f2fccb022796ce
igirb <139497578+igirb@users.noreply.github.com>
2024-11-03 18:37:48 +0100

    Create header component with basic functionality and definde its interface and prepare add new todo button

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import TodoList from './components/TodoList';
 import Body from './components/Body';
 import './styles/styles.css';
+import Header from './components/Header';
 
 
 const App: React.FC = () => {
   return (
     <Body>
+    <Header />
     <TodoList todos={[{title:"Pikk",done:false},{title:"Pakk",done:false},{title:"Pukk",done:true}]}  />  
     </Body>
   );
