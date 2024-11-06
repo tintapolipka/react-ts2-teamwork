97ca3a80ecbc21201fcdd7cf01f8fc77ac1726d0
igirb <139497578+igirb@users.noreply.github.com>
2024-11-06 16:33:20 +0100

    Create service module to handle todo commands and implement addTodo function

--- src/helpers/getTodo.ts
+++
@@ -1,9 +0,0 @@
-import { Todo } from "../interfaces/Todo";
-
-export default function getTodos(): Todo[] {
-    const todosJSON = localStorage.getItem('todos');
-    if (todosJSON) {
-        return JSON.parse(todosJSON);
-    }
-    return [];
-}
--- src/helpers/setTodo.ts
+++
@@ -1,7 +0,0 @@
-import { Todo } from "../interfaces/Todo";
-
-export default function setTodos(todos: Todo[]) {
-    const todosJSON = JSON.stringify(todos);
-
-    localStorage.setItem('todos', todosJSON);
-}
